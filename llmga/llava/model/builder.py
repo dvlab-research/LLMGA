0baa873d4a84bf1b48af739e83a764e2b3601884
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-07 16:20:07 +0800

    Update builder.py

--- llmga/llava/model/builder.py
+++ llmga/llava/model/builder.py
@@ -122,7 +122,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
 
     image_processor = None
 
-    if 'llava' in model_name.lower():
+    if 'llmga' in model_name.lower():
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
