297ada73da14b8828de5bc01e65f90b4b24f9774
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 20:51:28 +0800

    Update test-llmga13b-sd15-inpainting.sh

--- scripts/test-llmga13b-sd15-inpainting.sh
+++ scripts/test-llmga13b-sd15-inpainting.sh
@@ -1,7 +1,7 @@
 python3 -m llmga.serve.cli-sd15-inpainting \
     --model-path ./checkpoints/Inference/llmga-llama-2-13b-chat-full-finetune  \
     --sdmodel_id ./checkpoints/Inference/llmga-sd15-inpainting \
-    --save_path ./exp-inpainting/llmga13b-sd15 \
+    --save_path ./res/inpainting/llmga13b-sd15 \
     --image-file /PATHtoIMG \
     --mask-file /PATHtomask
 
