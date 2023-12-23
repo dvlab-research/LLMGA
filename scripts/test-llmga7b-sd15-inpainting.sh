80d7f89a2e498a37a0183d23581f9b18fd7440c9
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 20:53:27 +0800

    Update test-llmga7b-sd15-inpainting.sh

--- scripts/test-llmga7b-sd15-inpainting.sh
+++ scripts/test-llmga7b-sd15-inpainting.sh
@@ -1,7 +1,7 @@
 python3 -m llmga.serve.cli-sd15-inpainting \
-    --model-path ./checkpoints/llmga-llama-2-7b-chat-full-finetune  \
-    --sdmodel_id ./checkpoints/llmga-sd15-inpainting \
-    --save_path ./exp-inpainting/llmga7b-sd15 \
+    --model-path ./checkpoints/Inference/llmga-llama-2-7b-chat-full-finetune  \
+    --sdmodel_id ./checkpoints/Inference/llmga-sd15-inpainting \
+    --save_path ./ref/inpainting/llmga7b-sd15 \
     --image-file /PATHtoIMG \
     --mask-file /PATHtomask
 
