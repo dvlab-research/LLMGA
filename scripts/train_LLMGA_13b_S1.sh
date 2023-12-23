c297a557271cd1c2005f0527a869bee16ba54524
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 21:17:21 +0800

    Update train_LLMGA_13b_S1.sh

--- scripts/train_LLMGA_13b_S1.sh
+++ scripts/train_LLMGA_13b_S1.sh
@@ -4,21 +4,21 @@ MODEL_VERSION="llama-2-13b-chat"
 
 deepspeed --master_port=7000 llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path ./checkpoints/llava-$MODEL_VERSION-full-finetune \
+    --model_name_or_path ./checkpoints/Training/llava-$MODEL_VERSION-full-finetune \
     --version llava_llama_2 \
-    --data_path ./dataset/llava_instruct_150k.json \
-    --data_path2 ./dataset/COCO \
-    --data_path3 ./dataset/LLM-info \
-    --image_folder ./dataset/coco/train2017 \
-    --image_folder2 ./dataset/laion_aesthetics \
+    --data_path ./data/LLMGA-dataset/llava_instruct_150k.json \
+    --data_path2 ./data/LLMGA-dataset/coco2017_train.json \
+    --data_path3 ./data/LLMGA-dataset/LAION \
+    --image_folder ./data/COCO/train2017 \
+    --image_folder2 ./data/LAION-Aesthetic \
     --vision_tower openai/clip-vit-large-patch14 \
     --mm_vision_select_layer -2 \
     --mm_use_output_start_end True \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/llmga-$MODEL_VERSION-full-finetune \
-    --num_train_epochs 9 \
+    --output_dir ./work_dirs/llmga-$MODEL_VERSION-full-finetune \
+    --num_train_epochs 6 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
