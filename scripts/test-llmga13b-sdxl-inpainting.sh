f20a7284f3dec50986b5f6cbeb1b0b9a6e0c99ca
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 20:52:21 +0800

    Update test-llmga13b-sdxl-inpainting.sh

--- scripts/test-llmga13b-sdxl-inpainting.sh
+++ scripts/test-llmga13b-sdxl-inpainting.sh
@@ -1,6 +1,6 @@
 python3 -m llmga.serve.cli-sdxl-inpainting \
-    --model-path ./checkpoints/llmga-llama-2-13b-chat-full-finetune  \
-    --sdmodel_id ./checkpoints/llmga-sdxl-inpainting \
-    --save_path ./exp-inpainting/llmga13b-sdxl \
+    --model-path ./checkpoints/Inference/llmga-llama-2-13b-chat-full-finetune  \
+    --sdmodel_id ./checkpoints/Inference/llmga-sdxl-inpainting \
+    --save_path ./res/inpainting/llmga13b-sdxl \
     --image-file /PATHtoIMG \
     --mask-file /PATHtomask
