f9faafe9f93f580d9259c26b3f4dad031f4b7f81
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 21:39:10 +0800

    Update train_LLMGA_SDXL_S2_inpaint.sh

--- scripts/train_LLMGA_SDXL_S2_inpaint.sh
+++ scripts/train_LLMGA_SDXL_S2_inpaint.sh
@@ -2,7 +2,7 @@ export MODEL_NAME="diffusers/stable-diffusion-xl-1.0-inpainting-0.1" #"stability
 
 accelerate launch --main_process_port 1234 --mixed_precision "bf16" --multi_gpu  llmga/diffusers/train_text_to_image_sdxl_inpainting.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
-  --train_data_dir="./dataset/laion_aesthetics" \
+  --train_data_dir="./data" \
   --use_ema \
   --allow_tf32 \
   --resolution=1024 --center_crop --random_flip \
@@ -17,4 +17,4 @@ accelerate launch --main_process_port 1234 --mixed_precision "bf16" --multi_gpu 
   --checkpointing_steps 2000 \
   --checkpoints_total_limit 2 \
   --lr_warmup_steps=0 \
-  --output_dir="./checkpoints/llmga-sdxl-inpainting" \
+  --output_dir="./work_dirs/llmga-sdxl-inpainting" \
