c4351d17d30409a7bc88ead7d5343fe913db777a
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 21:37:07 +0800

    Update train_LLMGA_SD15_S2_inpaint.sh

--- scripts/train_LLMGA_SD15_S2_inpaint.sh
+++ scripts/train_LLMGA_SD15_S2_inpaint.sh
@@ -1,8 +1,7 @@
 export MODEL_NAME="runwayml/stable-diffusion-inpainting"
 accelerate launch --main_process_port 1234 --mixed_precision "bf16" --multi_gpu  llmga/diffusers/train_text_to_image_inpaint.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
-  --train_data_dir="./dataset/laion_aesthetics" \
-  --train_data_dir2="./dataset/coco/train2017" \
+  --train_data_dir="./data" \
   --use_ema \
   --allow_tf32 \
   --resolution=512 --center_crop --random_flip \
@@ -17,6 +16,6 @@ accelerate launch --main_process_port 1234 --mixed_precision "bf16" --multi_gpu 
   --checkpointing_steps 2000 \
   --checkpoints_total_limit 2 \
   --lr_warmup_steps=0 \
-  --output_dir="./checkpoints/llmga-sd15-inpainting" \
+  --output_dir="./work_dirs/llmga-sd15-inpainting" \
   --blank_mask_prob 0 \
 
