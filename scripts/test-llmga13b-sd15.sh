7d15055afecba4c8bae94b13164133a9aa08dba1
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 20:51:55 +0800

    Update test-llmga13b-sd15.sh

--- scripts/test-llmga13b-sd15.sh
+++ scripts/test-llmga13b-sd15.sh
@@ -1,5 +1,5 @@
 python3 -m llmga.serve.cli-sd15 \
-    --model-path ./checkpoints/Inference/llmga-llama-2-13b-chat-full-finetune  \
-    --sdmodel_id ./checkpoints/Inference/llmga-sd15-t2i \
-    --save_path ./exp/llmga13b-sd15 \
+    --model-path ./checkpoints/Inference/Inference/llmga-llama-2-13b-chat-full-finetune  \
+    --sdmodel_id ./checkpoints/Inference/Inference/llmga-sd15-t2i \
+    --save_path ./res/t2i/llmga13b-sd15 \
     --image-file /PATHtoIMG
