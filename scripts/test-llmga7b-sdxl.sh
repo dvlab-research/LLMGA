9701f24294a7e5e7fdecf2369fd9fce3f7c630a3
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-12-23 20:55:52 +0800

    Update test-llmga7b-sdxl.sh

--- scripts/test-llmga7b-sdxl.sh
+++ scripts/test-llmga7b-sdxl.sh
@@ -1,7 +1,7 @@
 python3 -m llmga.serve.cli-sdxl \
-    --model-path ./checkpoints/llmga-llama-2-7b-chat-full-finetune  \
-    --sdmodel_id ./checkpoints/llmga-sdxl-t2i \
-    --save_path ./exp/llmga7b-sdxl \
+    --model-path ./checkpoints/Inference/llmga-llama-2-7b-chat-full-finetune  \
+    --sdmodel_id ./checkpoints/Inference/llmga-sdxl-t2i \
+    --save_path ./res/t2i/llmga7b-sdxl \
     --image-file /PATHtoIMG
 
 
